5a5b7cdebedc00f6163549bcafd1fa15bd721971
Steven Stavrakis <s.a.stavrakis@gmail.com>
2024-06-13 17:17:42 -0400

    add comments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
   gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 
   $effect(() => {
+    // on first load, we need to create the smoother
       ScrollSmoother.create({
         wrapper: "#smooth-wrapper",
         content: "#smooth-content",
@@ -19,8 +20,10 @@
   $effect(() => {
     const smoother = ScrollSmoother.get();
     if ($navigating === null) {
+      // When navigating is null, that means we are no longer navigating; the page has been loaded
       ScrollSmoother.refresh();
     } else {
+      // when we start navigating, we scroll to the top otherwise we get some weird behavior
       if (!!smoother) {
       smoother.scrollTo(0)
       }
